c73b7ed08a04329657f2dda23480ea8e8545fa86
greed <rakib123kib.ra@gmail.com>
2026-04-13 21:21:44 +0600

    project initiated

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      {/* title */}
+      <h2>Friends to keep close in your life</h2>
+    </main>
+  );
+}
